7a329f2c960394caee070e61e622bbc47d77e2e2
sital002 <93700622+sital002@users.noreply.github.com>
2024-06-13 09:38:46 +0545

    feat: save the code locally

--- src/app/playground/_components/code-editor.tsx
+++ src/app/playground/_components/code-editor.tsx
@@ -9,42 +9,66 @@ import {
 } from "@/components/ui/select";
 import Editor from "@monaco-editor/react";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   MonacoThemes,
   defineTheme,
   monacoThemes,
 } from "../_utils/editor-theme";
-import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
+// import type * as monacoType from "monaco-editor";
 
+const defaultCode = "# this is a test";
 export function CodeEditor() {
-  const [value, setValue] = useState("# this is a test");
+  const [value, setValue] = useState(
+    localStorage.getItem("savedCode") || defaultCode
+  );
   const { theme: currentTheme } = useTheme();
+  // const editorRef = useRef<monacoType.editor.IStandaloneCodeEditor>();
   const params = useParams();
+  // const monacoRef = useRef<typeof import("monaco-editor")>();
   const [language, setLanguage] = useState(params.id as string);
 
   const handleEditorChange = (value: string | undefined) => {
     setValue(value || "");
   };
-  const savedTheme = useLocalStorage("theme");
-  const [theme, setTheme] = useState(
-    currentTheme === "dark" ? "vs-dark" : "light"
+  const [editorTheme, setEditorTheme] = useState(
+    localStorage.getItem("editorTheme") ?? "vs-dark"
   );
-
-  function handleThemeChange(theme: MonacoThemes) {
+  function handleThemeChange(theme: string) {
+    // setEditorTheme(theme);
+    localStorage.setItem("editorTheme", theme);
     if (["light", "vs-dark"].includes(theme)) {
-      setTheme(theme);
+      setEditorTheme(theme);
     } else {
-      defineTheme(theme).then((_) => setTheme(theme));
+      defineTheme(theme).then((_) => setEditorTheme(theme));
     }
   }
   useEffect(() => {
-    if (currentTheme) {
-      setTheme(currentTheme === "dark" ? "vs-dark" : "light");
+    const savedTheme = localStorage.getItem("editorTheme");
+    if (currentTheme && savedTheme) {
+      setEditorTheme(
+        savedTheme ? savedTheme : currentTheme === "dark" ? "vs-dark" : "light"
+      );
+      defineTheme(savedTheme).then((_) => setEditorTheme(savedTheme));
     }
   }, [currentTheme]);
+
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      // editorRef.current?.getAction("editor.action.formatDocument")?.run();
+      if (e.ctrlKey && e.key === "s") {
+        e.preventDefault();
+        localStorage.setItem("savedCode", value);
+        console.log("Code saved");
+        alert("Code saved");
+      }
+    }
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [value]);
 
   return (
     <div className="w-[60%]">
@@ -61,14 +85,14 @@ export function CodeEditor() {
             ))}
           </SelectContent>
         </Select>
-        <Select defaultValue="gasdrf" onValueChange={handleThemeChange}>
+        <Select defaultValue={editorTheme} onValueChange={handleThemeChange}>
           <SelectTrigger className="w-[180px]">
             <SelectValue defaultValue={"test"} />
           </SelectTrigger>
           <SelectContent>
-            {Object.keys(monacoThemes).map((theme) => (
-              <SelectItem value={theme} key={theme}>
-                {monacoThemes[theme as MonacoThemes]}
+            {monacoThemes.map((theme) => (
+              <SelectItem value={theme.value} key={theme.value}>
+                {theme.name}
               </SelectItem>
             ))}
           </SelectContent>
@@ -82,7 +106,7 @@ export function CodeEditor() {
         width={`100%`}
         language={language}
         value={value}
-        theme={theme}
+        theme={editorTheme}
         onChange={handleEditorChange}
       />
     </div>
--- src/app/playground/_utils/editor-theme.ts
+++ src/app/playground/_utils/editor-theme.ts
@@ -1,64 +1,208 @@
 import { loader } from "@monaco-editor/react";
 // import themes from "monaco-themes/themes/themelist.json";
 
-export const monacoThemes = {
-  active4d: "Active4D",
-  "all-hallows-eve": "All Hallows Eve",
-  amy: "Amy",
-  "birds-of-paradise": "Birds of Paradise",
-  blackboard: "Blackboard",
-  "brilliance-black": "Brilliance Black",
-  "brilliance-dull": "Brilliance Dull",
-  "chrome-devtools": "Chrome DevTools",
-  "clouds-midnight": "Clouds Midnight",
-  clouds: "Clouds",
-  cobalt: "Cobalt",
-  dawn: "Dawn",
-  dreamweaver: "Dreamweaver",
-  eiffel: "Eiffel",
-  "espresso-libre": "Espresso Libre",
-  github: "GitHub",
-  idle: "IDLE",
-  katzenmilch: "Katzenmilch",
-  "kuroir-theme": "Kuroir Theme",
-  lazy: "LAZY",
-  "magicwb--amiga-": "MagicWB (Amiga)",
-  "merbivore-soft": "Merbivore Soft",
-  merbivore: "Merbivore",
-  "monokai-bright": "Monokai Bright",
-  monokai: "Monokai",
-  "night-owl": "Night Owl",
-  "oceanic-next": "Oceanic Next",
-  "pastels-on-dark": "Pastels on Dark",
-  "slush-and-poppies": "Slush and Poppies",
-  "solarized-dark": "Solarized-dark",
-  "solarized-light": "Solarized-light",
-  spacecadet: "SpaceCadet",
-  sunburst: "Sunburst",
-  "textmate--mac-classic-": "Textmate (Mac Classic)",
-  "tomorrow-night-blue": "Tomorrow-Night-Blue",
-  "tomorrow-night-bright": "Tomorrow-Night-Bright",
-  "tomorrow-night-eighties": "Tomorrow-Night-Eighties",
-  "tomorrow-night": "Tomorrow-Night",
-  tomorrow: "Tomorrow",
-  twilight: "Twilight",
-  "upstream-sunburst": "Upstream Sunburst",
-  "vibrant-ink": "Vibrant Ink",
-  "xcode-default": "Xcode_default",
-  zenburnesque: "Zenburnesque",
-  iplastic: "iPlastic",
-  idlefingers: "idleFingers",
-  krtheme: "krTheme",
-  monoindustrial: "monoindustrial",
-};
+export const monacoThemes = [
+  {
+    name: "Active4D",
+    value: "active4d",
+  },
+  {
+    name: "All Hallows Eve",
+    value: "all-hallows-eve",
+  },
+  {
+    name: "Amy",
+    value: "amy",
+  },
+  {
+    name: "Birds of Paradise",
+    value: "birds-of-paradise",
+  },
+  {
+    name: "Blackboard",
+    value: "blackboard",
+  },
+  {
+    name: "Brilliance Black",
+    value: "brilliance-black",
+  },
+  {
+    name: "Brilliance Dull",
+    value: "brilliance-dull",
+  },
+  {
+    name: "Chrome DevTools",
+    value: "chrome-devtools",
+  },
+  {
+    name: "Clouds Midnight",
+    value: "clouds-midnight",
+  },
+  {
+    name: "Clouds",
+    value: "clouds",
+  },
+  {
+    name: "Cobalt",
+    value: "cobalt",
+  },
+  {
+    name: "Dawn",
+    value: "dawn",
+  },
+  {
+    name: "Dreamweaver",
+    value: "dreamweaver",
+  },
+  {
+    name: "Eiffel",
+    value: "eiffel",
+  },
+  {
+    name: "Espresso Libre",
+    value: "espresso-libre",
+  },
+  {
+    name: "GitHub",
+    value: "github",
+  },
+  {
+    name: "IDLE",
+    value: "idle",
+  },
+  {
+    name: "Katzenmilch",
+    value: "katzenmilch",
+  },
+  {
+    name: "Kuroir Theme",
+    value: "kuroir-theme",
+  },
+  {
+    name: "LAZY",
+    value: "lazy",
+  },
+  {
+    name: "MagicWB (Amiga)",
+    value: "magicwb--amiga-",
+  },
+  {
+    name: "Merbivore Soft",
+    value: "merbivore-soft",
+  },
+  {
+    name: "Merbivore",
+    value: "merbivore",
+  },
+  {
+    name: "Monokai Bright",
+    value: "monokai-bright",
+  },
+  {
+    name: "Monokai",
+    value: "monokai",
+  },
+  {
+    name: "Night Owl",
+    value: "night-owl",
+  },
+  {
+    name: "Oceanic Next",
+    value: "oceanic-next",
+  },
+  {
+    name: "Pastels on Dark",
+    value: "pastels-on-dark",
+  },
+  {
+    name: "Slush and Poppies",
+    value: "slush-and-poppies",
+  },
+  {
+    name: "Solarized-dark",
+    value: "solarized-dark",
+  },
+  {
+    name: "Solarized-light",
+    value: "solarized-light",
+  },
+  {
+    name: "SpaceCadet",
+    value: "spacecadet",
+  },
+  {
+    name: "Sunburst",
+    value: "sunburst",
+  },
+  {
+    name: "Textmate (Mac Classic)",
+    value: "textmate--mac-classic-",
+  },
+  {
+    name: "Tomorrow-Night-Blue",
+    value: "tomorrow-night-blue",
+  },
+  {
+    name: "Tomorrow-Night-Bright",
+    value: "tomorrow-night-bright",
+  },
+  {
+    name: "Tomorrow-Night-Eighties",
+    value: "tomorrow-night-eighties",
+  },
+  {
+    name: "Tomorrow-Night",
+    value: "tomorrow-night",
+  },
+  {
+    name: "Tomorrow",
+    value: "tomorrow",
+  },
+  {
+    name: "Twilight",
+    value: "twilight",
+  },
+  {
+    name: "Upstream Sunburst",
+    value: "upstream-sunburst",
+  },
+  {
+    name: "Vibrant Ink",
+    value: "vibrant-ink",
+  },
+  {
+    name: "Xcode_default",
+    value: "xcode-default",
+  },
+  {
+    name: "Zenburnesque",
+    value: "zenburnesque",
+  },
+  {
+    name: "iPlastic",
+    value: "iplastic",
+  },
+  {
+    name: "idleFingers",
+    value: "idlefingers",
+  },
+  {
+    name: "krTheme",
+    value: "krtheme",
+  },
+  {
+    name: "monoindustrial",
+    value: "monoindustrial",
+  },
+];
+export type MonacoThemes = (typeof monacoThemes)[number]["value"];
 
-export type MonacoThemes = keyof typeof monacoThemes;
-
-const defineTheme = (theme: MonacoThemes) => {
+const defineTheme = (theme: string) => {
+  const currentTheme = monacoThemes.find((t) => t.value === theme);
   return new Promise((res) => {
     Promise.all([
       loader.init(),
-      import(`monaco-themes/themes/${monacoThemes[theme]}.json`),
+      import(`monaco-themes/themes/${currentTheme?.name}.json`),
     ]).then(([monaco, themeData]) => {
       monaco.editor.defineTheme(theme, themeData);
       res("done");
